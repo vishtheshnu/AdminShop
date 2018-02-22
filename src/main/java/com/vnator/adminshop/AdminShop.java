497b3552027c34c96a2e56851439801a84c3310f
Vishnu Narayana <vishtheshnu@gmail.com>
2018-02-21 16:36:44 -0800

    Added server updating client on its balance on player login

--- src/main/java/com/vnator/adminshop/AdminShop.java
+++ src/main/java/com/vnator/adminshop/AdminShop.java
@@ -2,9 +2,13 @@ package com.vnator.adminshop;
 
 import com.google.common.eventbus.Subscribe;
 import com.vnator.adminshop.blocks.shop.ShopStock;
+import com.vnator.adminshop.capabilities.money.MoneyProvider;
 import com.vnator.adminshop.client.AdminshopTab;
+import com.vnator.adminshop.packets.PacketHandler;
+import com.vnator.adminshop.packets.PacketUpdateMoney;
 import com.vnator.adminshop.proxy.CommonProxy;
 import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
@@ -16,11 +20,13 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 
+@Mod.EventBusSubscriber
 @Mod(modid = AdminShop.MODID, name = AdminShop.NAME, version = AdminShop.VERSION)
 public class AdminShop
 {
@@ -69,6 +75,11 @@ public class AdminShop
 		ShopStock.setShopStockSell(ConfigHandler.createStringListSell(), ConfigHandler.createPriceListSell());
 	}
 
+	@SubscribeEvent
+	public static void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event){
+		float money = event.player.getCapability(MoneyProvider.MONEY_CAPABILITY, null).getMoney();
+		PacketHandler.INSTANCE.sendTo(new PacketUpdateMoney(money), (EntityPlayerMP)event.player);
+	}
 
 	@Mod.EventBusSubscriber
 	public static class RegistrationHandler{
--- src/main/java/com/vnator/adminshop/proxy/ServerProxy.java
+++ src/main/java/com/vnator/adminshop/proxy/ServerProxy.java
@@ -1,7 +1,12 @@
 package com.vnator.adminshop.proxy;
 
+import com.vnator.adminshop.capabilities.money.MoneyProvider;
+import com.vnator.adminshop.packets.PacketHandler;
+import com.vnator.adminshop.packets.PacketUpdateMoney;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
 @Mod.EventBusSubscriber(Side.SERVER)
