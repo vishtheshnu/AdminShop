ae9fead824b82e164a6964defdedb460931d3bae
Torrax <Carson.morris2@gmail.com>
2021-02-13 01:24:30 -0500

    Allow Rotation Shop Block

--- src/main/java/com/vnator/adminshop/blocks/shop/BlockShop.java
+++ src/main/java/com/vnator/adminshop/blocks/shop/BlockShop.java
@@ -1,5 +1,11 @@
 package com.vnator.adminshop.blocks.shop;
 
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.BlockStateContainer;
+
 import com.vnator.adminshop.AdminShop;
 import com.vnator.adminshop.ModGuiHandler;
 import com.vnator.adminshop.blocks.BlockTileEntity;
@@ -18,8 +24,11 @@ import javax.annotation.Nullable;
 
 public class BlockShop extends BlockTileEntity<TileEntityShop> {
 
-	public BlockShop(){
-		super(Material.ROCK, "shop");
+	public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
+    	private static boolean hasTileEntity;
+	
+	public BlockShop(){super(Material.ROCK, "shop");
+		setDefaultState(blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
 	}
 
 	@Override
@@ -43,6 +52,36 @@ public class BlockShop extends BlockTileEntity<TileEntityShop> {
 	@Nullable
 	@Override
 	public TileEntityShop createTileEntity(World world, IBlockState state){ return new TileEntityShop(); }
+
+	@Override
+	protected BlockStateContainer createBlockState() {
+		 return new BlockStateContainer(this, new IProperty[] { FACING});
+	}
+
+    @Override
+    public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY,
+			float hitZ, int meta, EntityLivingBase placer, EnumHand hand) {
+        return super.getStateForPlacement(world, pos, facing, hitX, hitY, hitZ, meta, placer, hand).withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta)
+    {
+        EnumFacing enumfacing = EnumFacing.getFront(meta);
+
+        if (enumfacing.getAxis() == EnumFacing.Axis.Y)
+        {
+            enumfacing = EnumFacing.NORTH;
+        }
+
+        return getDefaultState().withProperty(FACING, enumfacing);
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state)
+    {
+        return ((EnumFacing)state.getValue(FACING)).getIndex();
+    }
 
 	@Override
 	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand,
